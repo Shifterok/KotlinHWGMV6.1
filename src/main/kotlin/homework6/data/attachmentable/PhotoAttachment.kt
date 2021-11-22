7821fc24d5b0a7462f5ecefc104bd79f8b17d9e7
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-22 12:20:20 +0300

    Update PhotoAttachment.kt

--- src/main/kotlin/homework6/data/attachmentable/PhotoAttachment.kt
+++ src/main/kotlin/homework6/data/attachmentable/PhotoAttachment.kt
@@ -1,13 +1,17 @@
 package homework6.data.attachmentable
 
 data class PhotoAttachment(
-    override val typeAttachment: String = "Photo",
-    override val id: Int = -1,
-    val albumId: Int = -1,
-    override val idOwner: Int = -1,
-    val useId: Int = -1,
-    val text: String = "",
-    val date: Int = 0,
-    val width: Int = 0,
-    val height: Int = 0,
+    override val type: String = "Photo",
+    val photo: Photo = Photo {
+        val albumId: Int = -1
+        val idOwner: Int = -1
+        val useId: Int = -1
+        val text: String = ""
+        val date: Int = 0
+        val width: Int = 0
+        val height: Int = 0
+    }
 ) : Attachment
+
+class Photo(function: () -> Unit) {
+}
