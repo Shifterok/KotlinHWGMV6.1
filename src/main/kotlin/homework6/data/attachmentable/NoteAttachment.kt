87fa2ead3a792c57d97b8cb64c1281ffbc503f9e
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-22 12:19:56 +0300

    Update NoteAttachment.kt

--- src/main/kotlin/homework6/data/attachmentable/NoteAttachment.kt
+++ src/main/kotlin/homework6/data/attachmentable/NoteAttachment.kt
@@ -1,14 +1,18 @@
 package homework6.data.attachmentable
 
 data class NoteAttachmentAttachment(
-    override var typeAttachment: String = "важная заметка",
-    override var id: Int = 1,
-    override var idOwner: Int = 1,
-    val title: String = "название заметки",
-    val text: String = "о чем моя заметка",
-    val date: Int = 1,
-    val comments: Int = 100500,
-    val readComments: Int = 1,
-    val viewUrl: String = "ссылка на заметку"
-
+    override val type: String = "важная заметка",
+    val note: Note = Note {
+        val idOwner: Int = 1
+        val title: String = ""
+        val text: String = ""
+        val date: Int = 1
+        val comments: Int = 100500
+        val readComments: Int = 1
+        val viewUrl: String = ""
+    }
 ) : Attachment
+
+class Note(function: () -> Unit) {
+}
+
