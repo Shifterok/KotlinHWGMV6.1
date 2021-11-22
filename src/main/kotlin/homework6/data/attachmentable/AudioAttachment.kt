d20a2e5576786dbce14d2cca0b2d59b870032998
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-22 12:18:51 +0300

    Update AudioAttachment.kt

--- src/main/kotlin/homework6/data/attachmentable/AudioAttachment.kt
+++ src/main/kotlin/homework6/data/attachmentable/AudioAttachment.kt
@@ -1,17 +1,21 @@
 package homework6.data.attachmentable
 
-abstract class AudioAttachment(
-    val type: String = "хорошая песня",
-    override var id: Int = 1,
-    override var idOwner: Int = 1,
-    val artist: String = "автор",
-    val title: String = "название",
-    val duration: Int = 125,
-    val url: String = "ссылка",
-    val lyriksId: Int = 1,
-    val albumId: Int = 1,
-    val genreId: Int = 1,
-    val date: Int = 1,
-    val noSearch: Boolean = true,
-    val isHQ: Boolean = true
+data class AudioAttachment(
+    override val type: String = "Audio",
+    val audio: Audio = Audio {
+        val idOwner: Int = 1
+        val artist: String = ""
+        val title: String = ""
+        val duration: Int = 125
+        val url: String = ""
+        val lyriksId: Int = 1
+        val albumId: Int = 1
+        val genreId: Int = 1
+        val date: Int = 1
+        val noSearch: Boolean = true
+        val isHQ: Boolean = true
+    }
 ) : Attachment
+
+class Audio(function: () -> Unit) {
+}
