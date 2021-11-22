7f27f2108adaafdc998e566c80db49dd3c654c9b
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-22 12:20:58 +0300

    Update PostSeurceAttachment.kt

--- src/main/kotlin/homework6/data/attachmentable/PostSeurceAttachment.kt
+++ src/main/kotlin/homework6/data/attachmentable/PostSeurceAttachment.kt
@@ -1,15 +1,16 @@
 package homework6.data.attachmentable
 
-data class PostSource(
-    override val typeAttachment: String = "Источник поста",
 
-    val type: String = "название источника",
-    val platform: String = "андроид, только андроид!!!",
-    val date: Int = 1,
-    val url: String = "ссылка куда то"
-) : Attachment {
-    override val id: Int
-        get() = TODO("Not yet implemented")
-    override val idOwner: Int
-        get() = TODO("Not yet implemented")
+data class PostSource(
+    override val type: String = "Doc",
+    val source: Source = Source {
+        val idOwner: Int = 1
+        val type: String = ""
+        val platform: String = ""
+        val date: Int = 1
+        val url: String = ""
+    }
+) : Attachment
+
+class Source(function: () -> Unit) {
 }
