f2b93343e5f09c999bd8acee4e490650a399ee2a
Shifterok <91421090+Shifterok@users.noreply.github.com>
2021-11-22 12:19:21 +0300

    Update DocumentAttachment.kt

--- src/main/kotlin/homework6/data/attachmentable/DocumentAttachment.kt
+++ src/main/kotlin/homework6/data/attachmentable/DocumentAttachment.kt
@@ -1,15 +1,19 @@
 package homework6.data.attachmentable
 
 data class DocumentAttachment(
-    override var typeAttachment: String = "Doc",
-    override var id: Int = -1,
-    override var idOwner: Int,
-    val ownerId: Int = -1,
-    val title: String = "",
-    val size: Int = -1,
-    val ext: String = "",
-    val url: String = "",
-    val date: Int = -1,
-    val type: Int = 8,
-    val preview: DocPreview,
+    override val type: String = "Doc",
+    val doc: Doc = Doc {
+        val idOwner: Int
+        val ownerId: Int = -1
+        val title: String = ""
+        val size: Int = -1
+        val ext: String = ""
+        val url: String = ""
+        val date: Int = -1
+        val typeOfDoc: Int = 8
+        val preview: DocPreview
+    }
 ) : Attachment
+
+class Doc(function: () -> Unit) {
+}
